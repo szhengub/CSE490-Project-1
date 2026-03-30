156456ac57a5a0256c19bee8014a852652c378de
Nas <143642674+AriaNas202@users.noreply.github.com>
2026-03-29 21:08:56 -0400

    Wrote first draft control unit
    (Untested). Will change required control variables as needed as we need them in the circuit.

---
+++ controlUnit.v
@@ -0,0 +1,131 @@
+module controlUnit (opcode, funct, regdst, branch, memread, memtoreg, aluop, memwrite, alusrc, regwrite, jump, invertbranch);
+    input [15:12] opcode;
+    input [3:0] funct;
+
+    output reg regdst;  //source for the destination register (1 for R-type, 0 for I-type)
+    output reg branch; //are we branches? 1 for yes
+    output reg memread; //are we reading memory? 1 for yes
+    output reg memtoreg; //which value to write back to reg? 1 for memory, 0 for ALU 
+    output reg [1:0] aluop; //different for different instructions
+    output reg memwrite; //are we writing memory? 1 for yes
+    output reg alusrc; // source for alu operand 2, 1 for immediate, 0 for register 2
+    output reg regwrite; //are we writing back to register? 1 for yes
+    output reg jump; //are we unconditional jumping? 1 for yes
+    output reg invertbranch; //differentiate between beq and bne, 1 for bne, 0 for beq
+
+    always @(*) begin
+
+        //maybe set some default values here?
+        regdst = 0;
+        branch = 0;
+        memread = 0;
+        memtoreg = 0;
+        aluop = 2'b00;
+        memwrite = 0;
+        alusrc = 0;
+        regwrite = 0;
+        jump = 0;
+        invertbranch = 0;
+        
+        if (opcode != 4'b0000) begin //if the opcode ISNT R-type (handle all i/j type)
+
+            //lw
+            if (opcode == 4'b0001) begin 
+                regdst= 0;
+                branch= 0;
+                memread= 1;
+                memtoreg= 1;
+                aluop= 2'b00;
+                memwrite= 0;
+                alusrc= 1;
+                regwrite= 1;
+                jump= 0;
+                invertbranch = 0;
+            end
+            //sw
+            else if (opcode == 4'b0010) begin 
+                regdst= 0; //X
+                branch= 0;
+                memread= 0;
+                memtoreg= 1; //X
+                aluop= 2'b00;
+                memwrite= 1;
+                alusrc = 1;
+                regwrite = 0;
+                jump = 0;
+                invertbranch = 0;
+            end
+            //addi 
+            else if (opcode == 4'b0011) begin 
+                regdst= 0;
+                branch= 0;
+                memread= 0;
+                memtoreg= 0;
+                aluop= 2'b00;
+                memwrite= 0;
+                alusrc= 1;
+                regwrite= 1;
+                jump= 0;
+                invertbranch = 0;
+            end 
+            //beq 
+            else if (opcode == 4'b0100) begin 
+                regdst=0; //X
+                branch= 1;
+                memread= 0;
+                memtoreg= 0; //X
+                aluop= 2'b01;
+                memwrite= 0;
+                alusrc= 0;
+                regwrite= 0;
+                jump= 0;
+                invertbranch = 0;
+            end
+            //bne 
+            else if (opcode == 4'b0101) begin 
+                regdst=0; //X
+                branch= 1;
+                memread= 0;
+                memtoreg= 0; //X
+                aluop= 2'b01;
+                memwrite= 0;
+                alusrc= 0;
+                regwrite= 0;
+                jump= 0;
+                invertbranch = 1;
+            end 
+            //jmp (0110)
+            else if (opcode == 4'b0110) begin 
+                regdst=0; //X
+                branch= 0;
+                memread= 0;
+                memtoreg= 0; //X
+                aluop= 2'b01; //X
+                memwrite= 0;
+                alusrc= 0; //X
+                regwrite= 0;
+                jump= 1;
+                invertbranch = 0;
+            end  
+
+        end
+        else begin //if opcode is R-type (all R-type codes have the same variables)
+            regdst = 1;
+            branch = 0;
+            memread = 0;
+            memtoreg = 0;
+            aluop = 2'b10;
+            memwrite = 0;
+            alusrc = 0;
+            regwrite = 1;
+            jump = 0;
+            invertbranch = 0;
+        end
+      
+
+    end
+   
+ 
+endmodule
+
+
