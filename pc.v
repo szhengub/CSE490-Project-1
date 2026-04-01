e976e499e7cbdc562e768d544af51c5b6708d79d
Nas <143642674+AriaNas202@users.noreply.github.com>
2026-04-01 18:23:41 -0400

    Changed Variable names to be more clear
    Old variable names were too generic and confusing

--- pc.v
+++ pc.v
@@ -1,17 +1,17 @@
-module pc(clock, oldPc, reset, newPc);
+module pc(clock, pcPlusTwo, reset, currentPc);
     input clock; //is the clock for the pc (1 is up tic, 0 is down tic)
-    input [15:0] oldPc; //the PC which we calculated in the prev cycle
+    input [15:0] pcPlusTwo; //the PC which we calculated in the prev cycle
     input reset; //are we resetting the circuit? (1==yes, 0==no)
-    output reg [15:0] newPc; //the pc which we'll use in the next cycle
+    output reg [15:0] currentPc; //the pc which we'll use in the next cycle
 
     //updates the pc upon every clock tic
     always @(posedge clock) begin 
         if (reset == 1) begin 
             //the circuit reset, set the pc to 0
-            newPc <= 0;
+            currentPc <= 0;
         end else begin 
             //no reset happened, continue the circuit running as normal
-            newPc <= oldPc; //the old Pc we calculated is the current Pc we're using 
+            currentPc <= pcPlusTwo; //the PC+2 we calculated in the prev cycle is fed into instruction mem for this cycle
         end 
     end 
 
