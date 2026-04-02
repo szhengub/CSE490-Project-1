565f027d1abaec004515343f96915a0839dba59c
Nas <143642674+AriaNas202@users.noreply.github.com>
2026-04-02 18:23:05 -0400

    Added 12-to-16 bit sign extender module

--- signExtender.v
+++ signExtender.v
@@ -1,4 +1,4 @@
-module signextender(imm, extendedImm);
+module immediateSignExtender(imm, extendedImm);
     //entends a 4 bit immediate value into 16 bits 
 
     input [3:0] imm;
@@ -8,6 +8,20 @@ module signextender(imm, extendedImm);
     //takes MSB of immediate and replicates it 12 times
     //then concats it with the original immediate
     assign extendedImm= { {12{imm[3]}}, imm};
+
+
+endmodule
+
+module jumpSignExtender(imm, extendedImm);
+    //entends a 4 bit immediate value into 16 bits 
+
+    input [11:0] imm;
+    output [15:0] extendedImm;
+
+
+    //takes MSB of immediate and replicates it 12 times
+    //then concats it with the original immediate
+    assign extendedImm= { {4{imm[11]}}, imm};
 
 
 endmodule
