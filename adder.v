1d1b4ecfbe107e5fc26b8cf211157b8521fee08c
Nas <143642674+AriaNas202@users.noreply.github.com>
2026-04-05 23:01:49 -0400

    Added Missed Semicolon

--- adder.v
+++ adder.v
@@ -4,7 +4,7 @@ module adder(operand1, operand2, adderResult);
     input [15:0] operand2;
     output [15:0] adderResult;
 
-    assign adderResult= operand1 + operand2
+    assign adderResult= operand1 + operand2;
 
 
 endmodule
